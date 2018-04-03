030da0047465aad65d4b495d07feb29200f11cd0
belkin <belkin@belkins-mac-pro.local>
2018-04-03 14:00:29 -0700

    Initial Commit

---
+++ iOSSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSSample
+//
+//  Created by belkin on 4/3/18.
+//  Copyright © 2018 belkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
