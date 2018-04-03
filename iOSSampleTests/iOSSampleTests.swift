a6982cac21f2b42283ab2949950a2bc30d5f9d92
belkin <belkin@belkins-mac-pro.local>
2018-04-03 14:19:10 -0700

    sample error injected!

--- iOSSampleTests/iOSSampleTests.swift
+++ iOSSampleTests/iOSSampleTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class iOSSampleTests: XCTestCase {
     
-    //sample
+    //sample setup
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
